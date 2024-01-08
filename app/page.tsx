1c2cf6825bb7bf8152a549058cc0aae650367246
Chris Cardone <72090091+atenaschris@users.noreply.github.com>
2024-01-08 18:28:26 +0100

    added clerk webhooks to manage user creation, update and delete

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div className="flex flex-col gap-y4">
-      <h1>Only auth users can se this</h1>
+      <h1>Only auth users cat see this</h1>
       <UserButton afterSignOutUrl="/" />
     </div>
   );

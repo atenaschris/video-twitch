386ca589337ae8383a51482898c257fe2719fa47
Chris Cardone <72090091+atenaschris@users.noreply.github.com>
2024-01-08 15:48:32 +0100

    added prisma && user model

---
+++ lib/db.ts
@@ -0,0 +1,11 @@
+// Used to prevent the Hot reload(onlu used in development) to create a bunch of new prisma client every time.
+
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+export const db = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
